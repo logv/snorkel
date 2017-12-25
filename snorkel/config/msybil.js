3726da3e61d2d07037c50850204020aa8518ed90
okay <okay@chalk>
2017-12-24 16:07:56 -0800

    [msybil] update msybil default config

--- snorkel/config/msybil.js
+++ snorkel/config/msybil.js
@@ -1,7 +1,12 @@
 module.exports = {
   hostname: 'localhost',
   behind_proxy: false,
+  show_tour: true,
   config_driver: "linvo",
+  authorized_roles: "config/localhost.rbac",
+  analytics: {
+    enabled: true
+  },
   backend: {
     driver: "sybil",
     db: "snorkel",
