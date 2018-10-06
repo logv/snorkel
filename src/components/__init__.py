e0359ca2197902950aaac75320f9931589f68471
okay <okay@chalk>
2018-10-06 13:46:20 -0700

    [cleanup] move bootstrap files into vendor

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -9,6 +9,6 @@ from . import sf_components as sf
 
 def install(app):
     pudgy.use_jquery()
-    pudgy.add_to_prelude("bootstrap", os.path.join(app.static_folder, "bootstrap.min.js"))
+    pudgy.add_to_prelude("bootstrap", os.path.join(app.static_folder, "vendor/bootstrap.min.js"))
     pudgy.add_prelude_line("require('bootstrap')");
     app.after_request(pudgy.compress_request)
