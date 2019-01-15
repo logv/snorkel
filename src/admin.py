42e894311dbd9d35e3d7cea29a3d065c7fe38717
okay <okay@chalk>
2019-01-15 07:18:11 -0800

    [admin] require superuser login to view /admin page

--- src/admin.py
+++ src/admin.py
@@ -1,9 +1,11 @@
-from flask import redirect, url_for, request
+from flask import redirect, url_for, request, flash
 
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.peewee import ModelView
 
 from flask_security.core import current_user
+from flask_security import login_required
+from flask_admin import expose
 
 from . import models
 # set optional bootswatch theme
@@ -31,11 +33,21 @@ class AdminModelView(ModelView):
                 # login
                 return redirect(url_for('security.login', next=request.url))
 
+class MyAdminIndexView(AdminIndexView):
+    @expose('/')
+    @login_required
+    def index(self):
+        if current_user.has_role('superuser'):
+            return super(MyAdminIndexView,self).index()
+        else:
+            flash("You must be logged in as a superuser to access /admin")
+            return redirect("/")
+
 def install(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'flatly'
     app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
- 
-    admin = Admin(app, name='snorkelite', template_mode='bootstrap3')
+
+    admin = Admin(app, name='snorkelite', template_mode='bootstrap3', index_view=MyAdminIndexView())
     admin.add_view(AdminModelView(models.User, name='Users'))
     admin.add_view(AdminModelView(models.UserRoles, name='UserRoles'))
     admin.add_view(AdminModelView(models.Role, name='Roles'))
